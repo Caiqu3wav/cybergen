3d470706e9fc2737ae80d7652f8ecf47c0d260df
Caique <asapcaique7@gmail.com>
2024-09-28 14:27:00 -0300

    criação da tabela collections de nfts

--- backend/src/pkg/models/models.go
+++ backend/src/pkg/models/models.go
@@ -6,7 +6,7 @@ import (
 )
 
 type User struct {
-	ID		uuid.UUID	 `type:uuid;default:uuid_generate_v4();primaryKey"`
+	ID		uuid.UUID	 `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Name     string 	`gorm:"type:varchar(50);unique;not null"`
 	Email    string	 `gorm:"type:varchar(100);not null"`
 	Password string  `gorm:"type:varchar(60);not null"`
@@ -17,13 +17,27 @@ type User struct {
 	NFTs     []NFT  `gorm:"foreignKey:OwnerId"`
 }
 
+type Collection struct {
+	ID		uuid.UUID 	`gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name	string 	   `gorm:"type:varchar(100);not null"`
+	Description string `gorm:"type:text"`
+	ImageUrl string    `gorm:"type:varchar(255)"`
+    Tags    []string   `gorm:"type:text[]"`
+	Creator    User  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	CreatorId  uuid.UUID
+	NFTs	[]NFT  `gorm:"foreignKey:CollectionId"`
+	CreatedAt   time.Time `gorm:"autoCreateTime"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime"`
+}
+
 type NFT struct {
 	ID		uuid.UUID 	`gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Name	string 	   `gorm:"type:varchar(100);not null"`
 	Description string `gorm:"type:text"`
 	ImageUrl string    `gorm:"type:varchar(255)"`
+	Tags    []string   `gorm:"type:text[]"`
 	Owner    User  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	OwnerId  uint
+	OwnerId  uuid.UUID
 	Category     string    `gorm:"type:varchar(50)"` 
 	Attributes   string    `gorm:"type:jsonb"`       
 	Price        float64   `gorm:"type:decimal(10,2)"`
